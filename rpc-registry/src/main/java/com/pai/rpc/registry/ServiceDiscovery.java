89fc512012cf12a80cb6d28f3d062f0c486e946a
ZhangYantao <3454317160@qq.com>
2024-05-20 17:15:26 +0800

    merge module rpc-registry to module rpc-common

--- rpc-registry/src/main/java/com/pai/rpc/registry/ServiceDiscovery.java
+++
@@ -1,6 +0,0 @@
-package com.pai.rpc.registry;
-
-public interface ServiceDiscovery {
-
-    String discovery(String serviceName);
-}
--- rpc-registry/src/main/java/com/pai/rpc/registry/ServiceRegistry.java
+++
@@ -1,6 +0,0 @@
-package com.pai.rpc.registry;
-
-public interface ServiceRegistry {
-
-    void register(String serviceName, String serviceAddress);
-}
